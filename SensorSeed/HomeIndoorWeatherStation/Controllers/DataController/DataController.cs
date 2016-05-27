701a9bd5b4c5d80702857faf4e8a4a7d57501e40
William Quade <liam@quade.co>
2016-05-26 23:55:56 -0500

    Mostly working indoor weather station data retrieval code.

---
+++ SensorSeed/HomeIndoorWeatherStation/Controllers/DataController/DataController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HomeIndoorWeatherStation.Controllers.DataController
+{
+    public class DataController : Controller
+    {
+        [HttpPost]
+        public ActionResult AddData(string StationIP, string Temperature, string Humidity)
+        {
+            SensorSeedDataContext database = new SensorSeedDataContext();
+
+            string[] WeatherStationBasementRoomAddress = { "10.0.10.4", "/dht2-temperature", "/dht2-humidity", "basement" };
+            string[] WeatherStationLivingRoomAddress = { "10.0.10.5", "/dht2-temperature", "/dht2-humidity", "living room" };
+            string[] WeatherStationServerRoomAddress = { "10.0.10.6", "/dht-temperature", "/dht-humidity", "server room" };
+            string[] WeatherStationWilliamsRoomAddress = { "10.0.10.7", "/dht-temperature", "/dht-humidity", "William's room" };
+
+
+            if (StationIP == WeatherStationBasementRoomAddress[0])
+            {
+                HomeBasementRoomWeatherStationData data = new HomeBasementRoomWeatherStationData();
+                data.Id = Guid.NewGuid();
+                data.Timestamp = DateTime.UtcNow;
+                // Temperature
+                try
+                {
+                    decimal TemperatureDecimal = 0;
+                    TemperatureDecimal = Convert.ToDecimal(Temperature);
+                    data.Temperature = TemperatureDecimal;
+                }
+                catch (Exception exception) { }
+
+                // Humidity
+                try
+                {
+                    decimal HumidityDecimal = 0;
+                    HumidityDecimal = Convert.ToDecimal(Humidity);
+                    data.Humidity = HumidityDecimal;
+                }
+                catch (Exception exception) { }
+
+                database.HomeBasementRoomWeatherStationDatas.InsertOnSubmit(data);
+                database.SubmitChanges();
+            }
+            else if (StationIP == WeatherStationLivingRoomAddress[1])
+            {
+                HomeLivingRoomWeatherStationData data = new HomeLivingRoomWeatherStationData();
+                data.Id = Guid.NewGuid();
+                data.Timestamp = DateTime.UtcNow;
+                // Temperature
+                try
+                {
+                    decimal TemperatureDecimal = 0;
+                    TemperatureDecimal = Convert.ToDecimal(Temperature);
+                    data.Temperature = TemperatureDecimal;
+                }
+                catch (Exception exception) { }
+
+                // Humidity
+                try
+                {
+                    decimal HumidityDecimal = 0;
+                    HumidityDecimal = Convert.ToDecimal(Humidity);
+                    data.Humidity = HumidityDecimal;
+                }
+                catch (Exception exception) { }
+
+                database.HomeLivingRoomWeatherStationDatas.InsertOnSubmit(data);
+                database.SubmitChanges();
+            }
+            else if (StationIP == WeatherStationServerRoomAddress[1])
+            {
+                HomeServerRoomWeatherStationData data = new HomeServerRoomWeatherStationData();
+                data.Id = Guid.NewGuid();
+                data.Timestamp = DateTime.UtcNow;
+                // Temperature
+                try
+                {
+                    decimal TemperatureDecimal = 0;
+                    TemperatureDecimal = Convert.ToDecimal(Temperature);
+                    data.Temperature = TemperatureDecimal;
+                }
+                catch (Exception exception) { }
+
+                // Humidity
+                try
+                {
+                    decimal HumidityDecimal = 0;
+                    HumidityDecimal = Convert.ToDecimal(Humidity);
+                    data.Humidity = HumidityDecimal;
+                }
+                catch (Exception exception) { }
+
+                database.HomeServerRoomWeatherStationDatas.InsertOnSubmit(data);
+                database.SubmitChanges();
+            }
+            else if (StationIP == WeatherStationWilliamsRoomAddress[1])
+            {
+                HomeWilliamRoomWeatherStationData data = new HomeWilliamRoomWeatherStationData();
+                data.Id = Guid.NewGuid();
+                data.Timestamp = DateTime.UtcNow;
+                // Temperature
+                try
+                {
+                    decimal TemperatureDecimal = 0;
+                    TemperatureDecimal = Convert.ToDecimal(Temperature);
+                    data.Temperature = TemperatureDecimal;
+                }
+                catch (Exception exception) { }
+
+                // Humidity
+                try
+                {
+                    decimal HumidityDecimal = 0;
+                    HumidityDecimal = Convert.ToDecimal(Humidity);
+                    data.Humidity = HumidityDecimal;
+                }
+                catch (Exception exception) { }
+
+                database.HomeWilliamRoomWeatherStationDatas.InsertOnSubmit(data);
+                database.SubmitChanges();
+            }
+
+
+            JsonResult jsonResult = new JsonResult();
+            jsonResult.Data = "ok";
+
+            return jsonResult;
+        }
+    }
+}
--- SensorSeed/HomeIndoorWeatherStationDataService/Program.cs
+++ SensorSeed/HomeIndoorWeatherStationDataService/Program.cs
@@ -1,15 +1,148 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Cache;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace HomeIndoorWeatherStationDataService
 {
     class Program
     {
+
+        public static string[] WeatherStationBasementRoomAddress = { "10.0.10.4", "/dht2-temperature", "/dht2-humidity", "basement" };
+        public static string[] WeatherStationLivingRoomAddress = { "10.0.10.5", "/dht2-temperature", "/dht2-humidity", "living room"};
+        public static string[] WeatherStationServerRoomAddress = { "10.0.10.6", "/dht-temperature", "/dht-humidity", "server room"};
+        public static string[] WeatherStationWilliamsRoomAddress = { "10.0.10.7", "/dht-temperature", "/dht-humidity", "William's room" };
+
+
+        static double GetTemperature(string temperatureUrl)
+        {
+            string html = string.Empty;
+            try
+            {
+                HttpWebRequest temperatureRequest = (HttpWebRequest)WebRequest.Create(temperatureUrl);
+                temperatureRequest.CachePolicy = new HttpRequestCachePolicy(HttpRequestCacheLevel.NoCacheNoStore);
+                temperatureRequest.Timeout = 20000;
+
+                using (HttpWebResponse temperatureResponse = (HttpWebResponse)temperatureRequest.GetResponse())
+
+                using (Stream temperatureStream = temperatureResponse.GetResponseStream())
+                using (StreamReader temperatureReader = new StreamReader(temperatureStream))
+                {
+                    html = temperatureReader.ReadToEnd();
+                    if (html == "" || html == "nan")
+                    {
+                        return -10000;
+                    }
+                    return Convert.ToDouble(html);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error");
+                return -10000;
+            }
+        }
+
+        static double GetHumidity(string humidityUrl)
+        {
+            string html = string.Empty;
+            try
+            {
+                HttpWebRequest humidityRequest = (HttpWebRequest)WebRequest.Create(humidityUrl);
+                humidityRequest.CachePolicy = new HttpRequestCachePolicy(HttpRequestCacheLevel.NoCacheNoStore);
+                humidityRequest.Timeout = 20000;
+
+                using (HttpWebResponse humidityResponse = (HttpWebResponse)humidityRequest.GetResponse())
+
+                using (Stream humidityStream = humidityResponse.GetResponseStream())
+                using (StreamReader humidityReader = new StreamReader(humidityStream))
+                {
+                    html = humidityReader.ReadToEnd();
+                    if (html == "" || html == "nan")
+                    {
+                        return -10000;
+                    }
+                    return Convert.ToDouble(html);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error");
+                return -10000;
+            }
+        }
+
+        static void GetStationData(string[] WeatherStationAddress)
+        {
+
+            string temperatureUrl = @"http://" + WeatherStationAddress[0] + WeatherStationAddress[1];
+            string humidityUrl = @"http://" + WeatherStationAddress[0] + WeatherStationAddress[2];
+
+            bool tryAgain = true;
+            while (tryAgain)
+            {
+                DateTime currentTime = DateTime.Now;
+
+                Console.Write(currentTime.ToString());
+                Console.Write(":    Getting " + WeatherStationAddress[3] + " sensor data...");
+
+                var postData = new NameValueCollection();
+
+                double temperature1 = GetTemperature(temperatureUrl);
+                Thread.Sleep(3000); // the sensor needs time between readings
+                double temperature2 = GetTemperature(temperatureUrl);
+                Thread.Sleep(3000);
+                double temperature3 =  GetTemperature(temperatureUrl);
+                Thread.Sleep(3000);
+                string temperature = "";
+                if (temperature1 != -10000 && temperature2 != -10000 && temperature3 != -10000)
+                {
+                    temperature = ((temperature1 + temperature2 + temperature3)/3.0).ToString();
+                }
+
+                double humidity1 = GetTemperature(humidityUrl);
+                Thread.Sleep(3000);
+                double humidity2 = GetTemperature(humidityUrl);
+                Thread.Sleep(3000);
+                double humidity3 = GetTemperature(humidityUrl);
+                Thread.Sleep(3000);
+                string humidity = "";
+                if (humidity1 != -10000 && humidity2 != -10000 && humidity3 != -10000)
+                {
+                    humidity = ((humidity1 + humidity2 + humidity3) / 3.0).ToString();
+                }
+
+                postData["StationIP"] = WeatherStationAddress[0];
+                postData["Temperature"] = temperature;
+                postData["Humidity"] = humidity;
+
+                using (var client = new WebClient())
+                {
+                    var postResponse = client.UploadValues("http://localhost/SensorSeed/HomeIndoorWeatherStation/Data/AddData", postData);
+                    var postResponseString = Encoding.Default.GetString(postResponse);
+                    Console.WriteLine(postResponseString);
+                    tryAgain = false;
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
+            while (true)
+            {
+                GetStationData(WeatherStationBasementRoomAddress);
+                GetStationData(WeatherStationLivingRoomAddress);
+                GetStationData(WeatherStationServerRoomAddress);
+                GetStationData(WeatherStationWilliamsRoomAddress);
+
+                Thread.Sleep(TimeSpan.FromMinutes(5));
+            }
         }
     }
 }
